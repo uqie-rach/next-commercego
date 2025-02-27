128cd344617bcf71bb4be1c23ad569185f7f9a59
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-02-27 13:26:47 +0700

    add global context to root component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { SidebarProvider } from "@/context/SidebarContext";
+import { ThemeProvider } from "@/context/ThemeContext";
 
+const outfit = Outfit({
+  variable: "--font-outfit-sans",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -8,8 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`dark:bg-gray-900`}>
-        {children}
+      <body className={`${outfit.variable} dark:bg-gray-900`}>
+        <ThemeProvider>
+          <SidebarProvider>{children}</SidebarProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
