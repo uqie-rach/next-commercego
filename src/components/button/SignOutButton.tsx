83f84770750488ccf0544242854ced0220998d49
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-03-01 13:39:20 +0700

    fix odd behaviour on signout button

--- src/components/button/SignOutButton.tsx
+++ src/components/button/SignOutButton.tsx
@@ -2,7 +2,12 @@ import SignOutIcon from '@/icons/SignOutIcon';
 import { signOut } from 'next-auth/react';
 import React from 'react';
 
-const SignOutButton: React.FC = () => {
+interface SignOutButtonProps {
+  isExpanded: boolean;
+  isHovered: boolean;
+}
+
+const SignOutButton = ({ isExpanded, isHovered }: SignOutButtonProps) => {
   const handleSignOut = async () => {
     try {
       await signOut({ callbackUrl: "/signin" });
@@ -16,8 +21,16 @@ const SignOutButton: React.FC = () => {
       onClick={handleSignOut}
       className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
     >
-      <SignOutIcon />
-      Sign out
+      {
+        isExpanded && (
+          <>
+            <SignOutIcon
+            />
+            Sign Out
+          </>
+        ) || <SignOutIcon />
+        || isHovered && "Sign Out"
+      }
     </button>
   );
 };
