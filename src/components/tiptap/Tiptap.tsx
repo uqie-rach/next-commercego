341f9b921dec163ddf2ac37af1d880ead0b5504c
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-03-01 13:37:07 +0700

    feat: article editor ui

--- src/components/tiptap/Tiptap.tsx
+++ src/components/tiptap/Tiptap.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useCallback, useEffect, useState } from "react";
+import { useEditor, EditorContent, Node, mergeAttributes, FloatingMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import Bold from "@tiptap/extension-bold";
-import Italic from "@tiptap/extension-italic";
 import Underline from "@tiptap/extension-underline";
-import Strike from "@tiptap/extension-strike";
-import Highlight from "@tiptap/extension-highlight";
-import Code from "@tiptap/extension-code";
 import TextAlign from "@tiptap/extension-text-align";
-import BulletList from "@tiptap/extension-bullet-list";
-import ListItem from "@tiptap/extension-list-item";
-import Image from "@tiptap/extension-image";
-import "tailwindcss/tailwind.css";
+import Hightlight from "@tiptap/extension-highlight";
+import Code from "@tiptap/extension-code";
+import { all, createLowlight } from 'lowlight'
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import {
   Bold as BoldIcon,
   Italic as ItalicIcon,
@@ -26,84 +22,176 @@ import {
   List as BulletListIcon,
   Image as ImageIcon,
   HeadingIcon,
+  ListOrderedIcon,
+  QuoteIcon,
 } from "lucide-react";
 
+import { initialContent } from "@/lib/data/initialContent";
+import "tailwindcss/tailwind.css";
+import "@/styles/tiptap-editor.css"
+import Image from "@tiptap/extension-image";
+
+
+// Lowlight for code block syntax highlighting
+const lowlight = createLowlight(all)
+const Foo = Node.create({
+  name: 'foo',
+  group: 'inline',
+  inline: true,
+  parseHTML() {
+    return [
+      {
+        tag: 'span',
+        getAttrs: node => node.hasAttribute('data-foo') && null,
+      },
+    ]
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ['span', mergeAttributes({ 'data-foo': '', HTMLAttributes }), 'foo']
+  },
+  renderText() {
+    return 'foo'
+  },
+  addCommands() {
+    return {
+      insertFoo: () => ({ commands }) => {
+        return commands.insertContent({ type: this.name })
+      },
+    }
+  },
+})
+
 const Tiptap = () => {
+  const [headingLevel, setHeadingLevel] = useState(1);
+  const [isEditable, setIsEditable] = useState(true)
+
   const editor = useEditor({
     extensions: [
       StarterKit,
-      Bold,
-      Italic,
       Underline,
-      Strike,
-      Highlight,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      }),
       Code,
-      BulletList,
-      ListItem,
-      Image,
-      TextAlign.configure({ types: ["heading", "paragraph"] }),
+      Hightlight,
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+      Foo,
+      Image.configure({
+        allowBase64: true,
+        HTMLAttributes: {
+          class: 'mx-auto',
+        }
+      }),
     ],
-    content: "<p>Hello World! 🌎️</p>",
+    editorProps: {
+      attributes: {
+        class: "prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl focus:outline-none min-h-[250px]",
+      }
+    },
+    content: initialContent,
+    immediatelyRender: false,
   });
+
+  useEffect(() => {
+    if (editor) {
+      editor?.setEditable(isEditable)
+    }
+  }, [isEditable, editor])
+
+
+  const addImage = useCallback(() => {
+    const url = window.prompt('URL')
+
+    if (url) {
+      editor?.chain().focus().setImage({ src: url }).run()
+    }
+  }, [editor])
 
   if (!editor) return null;
 
-  const addImage = () => {
-    const url = prompt("Enter image URL:");
-    if (url) {
-      editor.chain().focus().setImage({ src: url }).run();
+  const saveContent = () => {
+    if (editor) {
+      console.log(editor.getJSON());
     }
   };
 
   return (
-    <div className="p-4 w-full mx-auto bg-gray-50 rounded-lg ">
+    <div className="p-4 w-full mx-auto bg-white rounded-lg relative">
       {/* Toolbar */}
-      <div className="flex space-x-2 border-b pb-2 mb-2">
-        <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={`p-2 border rounded flex items-center ${editor.isActive("bold") ? "bg-white" : ""
-            }`}
-        >
-          <BoldIcon size={18} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={`p-2 border rounded flex items-center ${editor.isActive("italic") ? "bg-white" : ""
-            }`}
-        >
-          <ItalicIcon size={18} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleUnderline().run()}
-          className={`p-2 border rounded flex items-center ${editor.isActive("underline") ? "bg-white" : ""
-            }`}
-        >
-          <UnderlineIcon size={18} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={`p-2 border rounded flex items-center ${editor.isActive("strike") ? "bg-white" : ""
-            }`}
-        >
-          <StrikeIcon size={18} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          className={`p-2 border rounded flex items-center ${editor.isActive("heading", { level: 2 }) ? "bg-white" : ""
-            }`}
-        >
-          <HeadingIcon size={18} />
-        </button>
+      <div className="flex flex-wrap gap-4 pb-2 mb-2 bg-gray-50 rounded-md p-2">
+        <ToolbarButton
+          command={() => editor.chain().focus().setParagraph().run()}
+          editor={editor}
+          icon={<p className="text-sm px-1.5 font-bold">P</p>}
+          isActive={editor.isActive("paragraph")}
+        />
+
+        <ToolbarButton
+          command={() => editor.chain().focus().toggleBold().run()}
+          editor={editor}
+          icon={<BoldIcon size={18} />}
+          isActive={editor.isActive("bold")}
+        />
+
+        <ToolbarButton
+          command={() => editor.chain().focus().toggleItalic().run()}
+          editor={editor}
+          icon={<ItalicIcon size={18} />}
+          isActive={editor.isActive("italic")}
+        />
+
+        <ToolbarButton
+          command={() => editor.chain().focus().toggleUnderline().run()}
+          editor={editor}
+          icon={<UnderlineIcon size={18} />}
+          isActive={editor.isActive("underline")}
+        />
+
+        <ToolbarButton
+          command={() => editor.chain().focus().toggleStrike().run()}
+          editor={editor}
+          icon={<StrikeIcon size={18} />}
+          isActive={editor.isActive("strike")}
+        />
+
+        <ToolbarButton
+          command={() => editor.chain().focus().toggleBlockquote().run()}
+          editor={editor}
+          icon={<QuoteIcon size={18} />}
+          isActive={editor.isActive("blockquote")}
+        />
+
+        {/* Toolbar heading */}
+        <div className="relative">
+          <ToolbarButton className={`p-2`} editor={editor} icon={<HeadingIcon size={18} />} isActive={editor.isActive("heading")} />
+          <select
+            value={headingLevel}
+            onChange={(e) => {
+              const level = Number(e.target.value);
+              setHeadingLevel(level);
+              editor.chain().focus().toggleHeading({ level }).run();
+            }}
+            className="absolute top-0 left-0 opacity-0 w-full h-full cursor-pointer appearance-none"
+          >
+            {[1, 2, 3, 4, 5, 6].map((level) => (
+              <option key={level} value={level}>
+                H{level}
+              </option>
+            ))}
+          </select>
+        </div>
 
         <ToolbarButton
           editor={editor}
-          command="toggleHighlight"
+          command={() => editor.chain().focus().toggleHighlight().run()}
           isActive={editor.isActive("highlight")}
           icon={<HighlightIcon size={18} />}
         />
         <ToolbarButton
           editor={editor}
-          command="toggleCode"
+          command={() => editor.chain().focus().toggleCode().run()}
           isActive={editor.isActive("code")}
           icon={<CodeIcon size={18} />}
         />
@@ -111,9 +199,17 @@ const Tiptap = () => {
         {/* Bullet List */}
         <ToolbarButton
           editor={editor}
-          command="toggleBulletList"
+          command={() => editor.chain().focus().toggleBulletList().run()}
           isActive={editor.isActive("bulletList")}
           icon={<BulletListIcon size={18} />}
+        />
+
+        {/* Ordered List */}
+        <ToolbarButton
+          editor={editor}
+          command={() => editor.chain().focus().toggleOrderedList().run()}
+          isActive={editor.isActive("orderedList")}
+          icon={<ListOrderedIcon size={18} />}
         />
 
         {/* Alignment */}
@@ -136,18 +232,141 @@ const Tiptap = () => {
           icon={<AlignRight size={18} />}
         />
 
+        {/* Code Block */}
+        <ToolbarButton
+          editor={editor}
+          command={() => editor.chain().focus().toggleCodeBlock().run()}
+          isActive={editor.isActive("codeBlock")}
+          icon={<CodeIcon size={18} />}
+        />
+
         {/* Image Upload */}
-        <button
-          onClick={addImage}
-          className="p-2 border rounded flex items-center"
-        >
-          <ImageIcon size={18} />
-        </button>
+        <ToolbarButton
+          editor={editor}
+          command={addImage}
+          isActive={false}
+          icon={<ImageIcon size={18} />}
+        />
       </div>
 
       {/* Editor */}
       <div className="border p-2 rounded bg-white min-h-[200px]">
-        <EditorContent editor={editor} className="outline-none" />
+        <div className="control-group">
+          {editor && <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
+            <div data-testid="floating-menu" className="floating-menu">
+              <ToolbarButton
+                command={() => editor.chain().focus().setParagraph().run()}
+                editor={editor}
+                icon={<p className="text-sm px-1.5 font-bold">P</p>}
+                isActive={editor.isActive("paragraph")}
+              />
+
+              <ToolbarButton
+                command={() => editor.chain().focus().toggleBold().run()}
+                editor={editor}
+                icon={<BoldIcon size={18} />}
+                isActive={editor.isActive("bold")}
+              />
+
+              <ToolbarButton
+                command={() => editor.chain().focus().toggleItalic().run()}
+                editor={editor}
+                icon={<ItalicIcon size={18} />}
+                isActive={editor.isActive("italic")}
+              />
+
+              <ToolbarButton
+                command={() => editor.chain().focus().toggleUnderline().run()}
+                editor={editor}
+                icon={<UnderlineIcon size={18} />}
+                isActive={editor.isActive("underline")}
+              />
+
+              <ToolbarButton
+                command={() => editor.chain().focus().toggleStrike().run()}
+                editor={editor}
+                icon={<StrikeIcon size={18} />}
+                isActive={editor.isActive("strike")}
+              />
+
+              <ToolbarButton
+                command={() => editor.chain().focus().toggleBlockquote().run()}
+                editor={editor}
+                icon={<QuoteIcon size={18} />}
+                isActive={editor.isActive("blockquote")}
+              />
+
+              {/* Toolbar heading */}
+              <div className="relative">
+                <ToolbarButton className={`p-2`} editor={editor} icon={<HeadingIcon size={18} />} isActive={editor.isActive("heading")} />
+                <select
+                  value={headingLevel}
+                  onChange={(e) => {
+                    const level = Number(e.target.value);
+                    setHeadingLevel(level);
+                    editor.chain().focus().toggleHeading({ level }).run();
+                  }}
+                  className="absolute top-0 left-0 opacity-0 w-full h-full cursor-pointer appearance-none"
+                >
+                  {[1, 2, 3, 4, 5, 6].map((level) => (
+                    <option key={level} value={level}>
+                      H{level}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <ToolbarButton
+                editor={editor}
+                command={() => editor.chain().focus().toggleHighlight().run()}
+                isActive={editor.isActive("highlight")}
+                icon={<HighlightIcon size={18} />}
+              />
+
+              {/* Bullet List */}
+              <ToolbarButton
+                editor={editor}
+                command={() => editor.chain().focus().toggleBulletList().run()}
+                isActive={editor.isActive("bulletList")}
+                icon={<BulletListIcon size={18} />}
+              />
+
+              {/* Ordered List */}
+              <ToolbarButton
+                editor={editor}
+                command={() => editor.chain().focus().toggleOrderedList().run()}
+                isActive={editor.isActive("orderedList")}
+                icon={<ListOrderedIcon size={18} />}
+              />
+
+              {/* Alignment */}
+              <ToolbarButton
+                editor={editor}
+                command={() => editor.chain().focus().setTextAlign("left").run()}
+                isActive={editor.isActive({ textAlign: "left" })}
+                icon={<AlignLeft size={18} />}
+              />
+              <ToolbarButton
+                editor={editor}
+                command={() => editor.chain().focus().setTextAlign("center").run()}
+                isActive={editor.isActive({ textAlign: "center" })}
+                icon={<AlignCenter size={18} />}
+              />
+              <ToolbarButton
+                editor={editor}
+                command={() => editor.chain().focus().setTextAlign("right").run()}
+                isActive={editor.isActive({ textAlign: "right" })}
+                icon={<AlignRight size={18} />}
+              />
+            </div>
+          </FloatingMenu>}
+        </div>
+        <EditorContent editor={editor} />
+      </div>
+
+      {/* Save Button */}
+      <div className="flex justify-end mt-6">
+        <button className="px-6 py-2 rounded-lg bg-brand-100 hover:bg-brand-200 text-base xl:text-lg text-brand-600" onClick={saveContent}>Save</button>
       </div>
     </div>
   );
@@ -159,7 +378,7 @@ export default Tiptap;
 const ToolbarButton = ({ editor, command, isActive, icon }: any) => (
   <button
     onClick={() => (typeof command === "string" ? editor.chain().focus()[command]().run() : command())}
-    className={`p-2 border rounded flex items-center ${isActive ? "bg-gray-300" : ""
+    className={`p-2 border rounded flex items-center bg-white ${isActive ? "bg-brand-200 text-brand-500" : ""
       }`}
   >
     {icon}
---
+++ src/lib/data/initialContent.ts
@@ -0,0 +1,262 @@
+export const initialContent = {
+  "type": "doc",
+  "content": [
+    {
+      "type": "heading",
+      "attrs": {
+        "textAlign": null,
+        "level": 1
+      },
+      "content": [
+        {
+          "type": "text",
+          "text": "Welcome to Google Developer Groups On Campus Maliki Dev Hub!"
+        }
+      ]
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": null
+      },
+      "content": [
+        {
+          "type": "text",
+          "text": "This is an example document that showcases all the supported features of this editor."
+        }
+      ]
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": null
+      },
+      "content": [
+        {
+          "type": "text",
+          "text": "You can use "
+        },
+        {
+          "type": "text",
+          "marks": [
+            {
+              "type": "bold"
+            }
+          ],
+          "text": "bold"
+        },
+        {
+          "type": "text",
+          "text": ", "
+        },
+        {
+          "type": "text",
+          "marks": [
+            {
+              "type": "italic"
+            }
+          ],
+          "text": "italic"
+        },
+        {
+          "type": "text",
+          "text": ", "
+        },
+        {
+          "type": "text",
+          "marks": [
+            {
+              "type": "underline"
+            }
+          ],
+          "text": "underline"
+        },
+        {
+          "type": "text",
+          "text": ", and "
+        },
+        {
+          "type": "text",
+          "marks": [
+            {
+              "type": "strike"
+            }
+          ],
+          "text": "strikethrough"
+        },
+        {
+          "type": "text",
+          "text": " text styles."
+        }
+      ]
+    },
+    {
+      "type": "blockquote",
+      "content": [
+        {
+          "type": "text",
+          "text": "This is a blockquote."
+        }
+      ]
+    },
+    {
+      "type": "bulletList",
+      "content": [
+        {
+          "type": "listItem",
+          "content": [
+            {
+              "type": "paragraph",
+              "attrs": {
+                "textAlign": null
+              },
+              "content": [
+                {
+                  "type": "text",
+                  "text": "Bullet point 1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "listItem",
+          "content": [
+            {
+              "type": "paragraph",
+              "attrs": {
+                "textAlign": null
+              },
+              "content": [
+                {
+                  "type": "text",
+                  "text": "Bullet point 2"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "type": "orderedList",
+      "attrs": {
+        "start": 1
+      },
+      "content": [
+        {
+          "type": "listItem",
+          "content": [
+            {
+              "type": "paragraph",
+              "attrs": {
+                "textAlign": null
+              },
+              "content": [
+                {
+                  "type": "text",
+                  "text": "Ordered item 1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "listItem",
+          "content": [
+            {
+              "type": "paragraph",
+              "attrs": {
+                "textAlign": null
+              },
+              "content": [
+                {
+                  "type": "text",
+                  "text": "Ordered item 2"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "type": "codeBlock",
+      "attrs": {
+        "language": "javascript"
+      },
+      "content": [
+        {
+          "type": "text",
+          "text": "console.log('Hello, World!');"
+        }
+      ]
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": null
+      },
+      "content": [
+        {
+          "type": "text",
+          "marks": [
+            {
+              "type": "highlight"
+            }
+          ],
+          "text": "This text is highlighted!"
+        }
+      ]
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": "center"
+      },
+      "content": [
+        {
+          "type": "text",
+          "text": "This text is centered."
+        }
+      ]
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": null
+      },
+      "content": [
+        {
+          "type": "foo"
+        }
+      ]
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": null
+      }
+    },
+    {
+      "type": "image",
+      "attrs": {
+        "src": "https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/how-to-make-a-url-for-a-picture-on-your-computer-4.jpg",
+        "alt": null,
+        "title": null
+      }
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": null
+      }
+    },
+    {
+      "type": "paragraph",
+      "attrs": {
+        "textAlign": null
+      }
+    }
+  ]
+}
