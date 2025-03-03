e0262c078e70ac9397021b067ec5cedc7af8a394
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-03-03 23:35:33 +0700

    add validation for the create article

---
+++ src/lib/validations/article.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const articleSchema = z.object({
+  title: z.string().min(5, "Title must have at least 5 characters").max(100, "Max. 100 Characters").nonempty(),
+  content: z.string().refine(
+    (data) => {
+      try {
+        JSON.parse(data); // Pastikan content adalah JSON valid
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    { message: "Content must be a valid JSON" }
+  ),
+  userId: z.string().cuid("Invalid UserID"), // Jika menggunakan UUID
+});
